1faa8d4cf1122162912150b2a5d4d3b1609bbbf7
Henri Chataing <henrichataing@google.com>
2024-09-25 23:47:54 +0000

    system/bta/av: Fix -Wmissing-prototypes errors
    Bug: 369381361
    Test: m com.android.btservices
    Flag: EXEMPT, minor fixes
    Change-Id: Ia8e80b055d1a948239951d3bd5b313ff71778b44

--- system/bta/av/bta_av_act.cc
+++ system/bta/av/bta_av_act.cc
@@ -49,9 +49,6 @@
 #include "stack/include/sdp_status.h"
 #include "types/raw_address.h"
 
-// TODO(b/369381361) Enfore -Wmissing-prototypes
-#pragma GCC diagnostic ignored "-Wmissing-prototypes"
-
 using namespace bluetooth::legacy::stack::sdp;
 using namespace bluetooth;
 
@@ -828,7 +825,7 @@ static tAVRC_STS bta_av_chk_notif_evt_id(tAVRC_MSG_VENDOR* p_vendor) {
   return status;
 }
 
-void bta_av_proc_rsp(tAVRC_RESPONSE* p_rc_rsp) {
+static void bta_av_proc_rsp(tAVRC_RESPONSE* p_rc_rsp) {
   uint16_t rc_ver = 0x105;
   const tBTA_AV_CFG* p_src_cfg = NULL;
   if (rc_ver != 0x103) {
@@ -862,8 +859,8 @@ void bta_av_proc_rsp(tAVRC_RESPONSE* p_rc_rsp) {
  * Returns          true to respond immediately
  *
  ******************************************************************************/
-tBTA_AV_EVT bta_av_proc_meta_cmd(tAVRC_RESPONSE* p_rc_rsp, tBTA_AV_RC_MSG* p_msg,
-                                 uint8_t* p_ctype) {
+static tBTA_AV_EVT bta_av_proc_meta_cmd(tAVRC_RESPONSE* p_rc_rsp, tBTA_AV_RC_MSG* p_msg,
+                                        uint8_t* p_ctype) {
   tBTA_AV_EVT evt = BTA_AV_META_MSG_EVT;
   uint8_t u8, pdu, *p;
   uint16_t u16;
@@ -1785,7 +1782,7 @@ static void bta_av_store_peer_rc_version() {
  * Returns          tBTA_AV_FEAT peer device feature mask
  *
  ******************************************************************************/
-tBTA_AV_FEAT bta_av_check_peer_features(uint16_t service_uuid) {
+static tBTA_AV_FEAT bta_av_check_peer_features(uint16_t service_uuid) {
   tBTA_AV_FEAT peer_features = 0;
   tBTA_AV_CB* p_cb = &bta_av_cb;
   tSDP_DISC_REC* p_rec = NULL;
@@ -1860,7 +1857,7 @@ tBTA_AV_FEAT bta_av_check_peer_features(uint16_t service_uuid) {
  * Returns          tBTA_AV_FEAT peer device feature mask
  *
  ******************************************************************************/
-tBTA_AV_FEAT bta_avk_check_peer_features(uint16_t service_uuid) {
+static tBTA_AV_FEAT bta_avk_check_peer_features(uint16_t service_uuid) {
   tBTA_AV_FEAT peer_features = 0;
   tBTA_AV_CB* p_cb = &bta_av_cb;
 
@@ -1949,7 +1946,7 @@ tBTA_AV_FEAT bta_avk_check_peer_features(uint16_t service_uuid) {
  *                  one does not exist.
  *
  *****************************************************************************/
-uint16_t bta_avk_get_cover_art_psm() {
+static uint16_t bta_avk_get_cover_art_psm() {
   log::verbose("searching for cover art psm");
   /* Cover Art L2CAP PSM is only available on a target device */
   tBTA_AV_CB* p_cb = &bta_av_cb;
@@ -2031,7 +2028,7 @@ uint16_t bta_avk_get_cover_art_psm() {
   return 0x0000;
 }
 
-void bta_av_rc_disc_done_all(tBTA_AV_DATA* /* p_data */) {
+static void bta_av_rc_disc_done_all(tBTA_AV_DATA* /* p_data */) {
   tBTA_AV_CB* p_cb = &bta_av_cb;
   tBTA_AV_SCB* p_scb = NULL;
   tBTA_AV_LCB* p_lcb;
