0d60b024d6874ce028c43a953f5b53bc47b87e1f
Michael Sun <michaelfsun@google.com>
2024-10-15 18:02:52 +0000

    floss: Add on_device_connection_failed callback
    Add a callback to immediately notify the browser that a pending
    connection attempt failed.
    
    Uprev btmanagerd version to 0.7.1,  this is backward compatible API change.
    
    Bug: 353527430
    Test: mmm packages/modules/Bluetooth
    Flag: EXEMPT, Floss only change
    
    Change-Id: I27cc51c76731c8d9113ad5bf025a14bec48d27ca

--- system/gd/rust/linux/stack/src/bluetooth.rs
+++ system/gd/rust/linux/stack/src/bluetooth.rs
@@ -574,6 +574,9 @@ pub trait IBluetoothConnectionCallback: RPCProxy {
 
     /// Notification sent when a remote device completes HCI disconnection.
     fn on_device_disconnected(&mut self, remote_device: BluetoothDevice);
+
+    /// Notification sent when a remote device fails to complete HCI connection.
+    fn on_device_connection_failed(&mut self, remote_device: BluetoothDevice, status: BtStatus);
 }
 
 /// Implementation of the adapter API.
@@ -2061,6 +2064,12 @@ impl BtifBluetoothCallbacks for Bluetooth {
                 conn_direction,
                 hci_reason,
             );
+            self.connection_callbacks.for_all_callbacks(|callback| {
+                callback.on_device_connection_failed(
+                    BluetoothDevice::new(addr, String::from("")),
+                    status,
+                );
+            });
             return;
         }
 
