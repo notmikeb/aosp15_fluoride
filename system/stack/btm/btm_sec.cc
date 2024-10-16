874c27d16f6f882c671064e424d5d2a2e9d4eee9
Ryo Hashimoto <hashimoto@google.com>
2024-10-16 16:02:30 +0900

    Pass appropriate types to base::StringPrintf
    Bug: 367200458
    Flag: EXEMPT, no logical change
    Test: mmm packages/modules/Bluetooth
    Change-Id: Ib70bd61002be56a52f29658e86dd5dab0aa5ac30

--- system/stack/btm/btm_sec.cc
+++ system/stack/btm/btm_sec.cc
@@ -2159,7 +2159,7 @@ tBTM_SEC_DEV_REC* btm_rnr_add_name_to_security_record(const RawAddress* p_bd_add
   BTM_LogHistory(
           kBtmLogTag, (p_bd_addr) ? *p_bd_addr : RawAddress::kEmpty, "RNR complete",
           base::StringPrintf("hci_status:%s name:%s", hci_error_code_text(hci_status).c_str(),
-                             PRIVATE_NAME(p_bd_name)));
+                             PRIVATE_NAME(reinterpret_cast<char const*>(p_bd_name))));
 
   if (p_dev_rec == nullptr) {
     // We need to send the callbacks to complete the RNR cycle despite failure
