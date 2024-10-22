miyu-xu/packages-modules-Bluetooth

Make it obvious that connection_manager is separate from GATT
Even though it now shares client identifiers with GATT, the plan is to
have it as independent piece of stack.

connection_manager is also used from L2CAP, and it looks improper when
l2cap relies on some GATT related things.

Test: mma -j32
Bug: 372202918
Flag: EXEMPT, just renaming/moving code
Change-Id: I7ae54072f140c360edac64ab4e1c9bcef72b1695

system/bta/dm/bta_dm_act.cc
@@ -52,7 +52,7 @@
 #include "main/shim/entry.h"
 #include "osi/include/allocator.h"
 #include "osi/include/properties.h"
-#include "stack/gatt/connection_manager.h"
+#include "stack/conn_mgr/connection_manager.h"
 #include "stack/include/acl_api.h"
 #include "stack/include/bt_hdr.h"
 #include "stack/include/bt_types.h"

system/btif/src/bluetooth.cc
@@ -99,7 +99,7 @@
 #include "osi/include/wakelock.h"
 #include "stack/btm/btm_dev.h"
 #include "stack/btm/btm_sco_hfp_hal.h"
-#include "stack/gatt/connection_manager.h"
+#include "stack/conn_mgr/connection_manager.h"
 #include "stack/include/a2dp_api.h"
 #include "stack/include/avdt_api.h"
 #include "stack/include/btm_client_interface.h"

system/stack/acl/ble_acl.cc
@@ -25,7 +25,7 @@
 #include "stack/btm/btm_dev.h"
 #include "stack/btm/btm_int_types.h"
 #include "stack/btm/btm_sec.h"
-#include "stack/gatt/connection_manager.h"
+#include "stack/conn_mgr/connection_manager.h"
 #include "stack/include/acl_api.h"
 #include "stack/include/btm_ble_addr.h"
 #include "stack/include/btm_ble_privacy.h"

system/stack/gatt/gatt_api.cc
@@ -37,7 +37,7 @@
 #include "osi/include/allocator.h"
 #include "stack/arbiter/acl_arbiter.h"
 #include "stack/btm/btm_dev.h"
-#include "stack/gatt/connection_manager.h"
+#include "stack/conn_mgr/connection_manager.h"
 #include "stack/gatt/gatt_int.h"
 #include "stack/include/ais_api.h"
 #include "stack/include/bt_hdr.h"

system/stack/gatt/gatt_utils.cc
@@ -38,8 +38,8 @@
 #include "osi/include/properties.h"
 #include "stack/btm/btm_dev.h"
 #include "stack/btm/btm_sec.h"
+#include "stack/conn_mgr/connection_manager.h"
 #include "stack/eatt/eatt.h"
-#include "stack/gatt/connection_manager.h"
 #include "stack/gatt/gatt_int.h"
 #include "stack/include/bt_hdr.h"
 #include "stack/include/bt_psm_types.h"

system/stack/l2cap/l2c_ble.cc
@@ -44,7 +44,7 @@
 #include "stack/btm/btm_int_types.h"
 #include "stack/btm/btm_sec.h"
 #include "stack/btm/btm_sec_int_types.h"
-#include "stack/gatt/connection_manager.h"
+#include "stack/conn_mgr/connection_manager.h"
 #include "stack/include/acl_api.h"
 #include "stack/include/bt_psm_types.h"
 #include "stack/include/bt_types.h"

system/stack/test/connection_manager_test.cc
@@ -1,3 +1,5 @@
+#include "stack/conn_mgr/connection_manager.h"
+
 #include <base/bind_helpers.h>
 #include <base/functional/bind.h>
 #include <base/functional/callback.h>
@@ -11,7 +13,6 @@
 #include "osi/test/alarm_mock.h"
 #include "security_device_record.h"
 #include "stack/btm/neighbor_inquiry.h"
-#include "stack/gatt/connection_manager.h"
 
 // TODO(b/369381361) Enfore -Wmissing-prototypes
 #pragma GCC diagnostic ignored "-Wmissing-prototypes"

notmikeb/aosp15_fluoride

system/bta/dm/bta_dm_act.cc
@@ -52,7 +52,7 @@
 #include "main/shim/entry.h"
 #include "osi/include/allocator.h"
 #include "osi/include/properties.h"
-#include "stack/gatt/connection_manager.h"
+#include "stack/connection_manager/connection_manager.h"
 #include "stack/include/acl_api.h"
 #include "stack/include/bt_hdr.h"
 #include "stack/include/bt_types.h"

system/btif/src/bluetooth.cc
@@ -100,7 +100,7 @@
 #include "osi/include/wakelock.h"
 #include "stack/btm/btm_dev.h"
 #include "stack/btm/btm_sco_hfp_hal.h"
-#include "stack/gatt/connection_manager.h"
+#include "stack/connection_manager/connection_manager.h"
 #include "stack/include/a2dp_api.h"
 #include "stack/include/avdt_api.h"
 #include "stack/include/btm_client_interface.h"

system/stack/acl/ble_acl.cc
@@ -25,7 +25,7 @@
 #include "stack/btm/btm_dev.h"
 #include "stack/btm/btm_int_types.h"
 #include "stack/btm/btm_sec.h"
-#include "stack/gatt/connection_manager.h"
+#include "stack/connection_manager/connection_manager.h"
 #include "stack/include/acl_api.h"
 #include "stack/include/btm_ble_addr.h"
 #include "stack/include/btm_ble_privacy.h"

system/stack/gatt/gatt_api.cc
@@ -37,7 +37,7 @@
 #include "osi/include/allocator.h"
 #include "stack/arbiter/acl_arbiter.h"
 #include "stack/btm/btm_dev.h"
-#include "stack/gatt/connection_manager.h"
+#include "stack/connection_manager/connection_manager.h"
 #include "stack/gatt/gatt_int.h"
 #include "stack/include/ais_api.h"
 #include "stack/include/bt_hdr.h"

system/stack/gatt/gatt_utils.cc
@@ -38,8 +38,8 @@
 #include "osi/include/properties.h"
 #include "stack/btm/btm_dev.h"
 #include "stack/btm/btm_sec.h"
+#include "stack/connection_manager/connection_manager.h"
 #include "stack/eatt/eatt.h"
-#include "stack/gatt/connection_manager.h"
 #include "stack/gatt/gatt_int.h"
 #include "stack/include/bt_hdr.h"
 #include "stack/include/bt_psm_types.h"

system/stack/l2cap/l2c_ble.cc
@@ -44,7 +44,7 @@
 #include "stack/btm/btm_int_types.h"
 #include "stack/btm/btm_sec.h"
 #include "stack/btm/btm_sec_int_types.h"
-#include "stack/gatt/connection_manager.h"
+#include "stack/connection_manager/connection_manager.h"
 #include "stack/include/acl_api.h"
 #include "stack/include/bt_psm_types.h"
 #include "stack/include/bt_types.h"

system/stack/test/connection_manager_test.cc
@@ -1,3 +1,5 @@
+#include "stack/connection_manager/connection_manager.h"
+
 #include <base/bind_helpers.h>
 #include <base/functional/bind.h>
 #include <base/functional/callback.h>
@@ -11,7 +13,6 @@
 #include "osi/test/alarm_mock.h"
 #include "security_device_record.h"
 #include "stack/btm/neighbor_inquiry.h"
-#include "stack/gatt/connection_manager.h"
 
 // TODO(b/369381361) Enfore -Wmissing-prototypes
 #pragma GCC diagnostic ignored "-Wmissing-prototypes"