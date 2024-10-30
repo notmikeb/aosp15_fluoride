944a9dcac3030a92d523f59502ac64d4a7ee7e1b
Chris Manton <cmanton@google.com>
2024-10-30 14:41:49 -0700

    stack::avrc Check alarm state before setting
    Bug: 343127654
    Test: atest net_test_stack
    Flag: EXEMPT, Null pointer exemption
    Change-Id: I5ede451510ee749b8cfde953d06929f19ec4b9b1

--- system/test/mock/mock_stack_avrc_api.cc
+++ system/test/mock/mock_stack_avrc_api.cc
@@ -77,6 +77,3 @@ uint16_t AVRC_PassRsp(uint8_t /* handle */, uint8_t /* label */, tAVRC_MSG_PASS*
 }
 void avrc_flush_cmd_q(uint8_t /* handle */) { inc_func_call_count(__func__); }
 void avrc_send_next_vendor_cmd(uint8_t /* handle */) { inc_func_call_count(__func__); }
-void avrc_start_cmd_timer(uint8_t /* handle */, uint8_t /* label */, uint8_t /* msg_mask */) {
-  inc_func_call_count(__func__);
-}
