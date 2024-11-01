miyu-xu/packages-modules-Bluetooth

AICS: add aidl definition
AudioInputType is a bluetooth generic definition.
Status/GainMode/Mute are aics specific

Bug: 372328699
Flag: com.android.bluetooth.flags.aics_api
Test: m Bluetooth
Change-Id: I3f309b15c6a830a41fde03dd5c4e9f85fd757913

android/app/aidl/android/bluetooth/aics/AudioInputStatus.aidl
@@ -1,0 +1,29 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.bluetooth.aics;
+
+/**
+ * See Audio Input Control Service 1.0 - 3.4 Audio Input Status
+ * {@hide}
+ */
+@JavaDerive(toString = true)
+@Backing(type="byte")
+enum AudioInputStatus {
+    INACTIVE = 0x00,
+    ACTIVE = 0x01,
+    // RFU 0x02 - 0xFF
+}

android/app/aidl/android/bluetooth/aics/GainMode.aidl
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.bluetooth.aics;
+
+/**
+ * See Audio Input Control Service 1.0 - 2.2.1.3. Gain_Mode field
+ * {@hide}
+ */
+@JavaDerive(toString = true)
+@Backing(type="byte")
+enum GainMode {
+    MANUAL_ONLY = 0x00,
+    AUTOMATIC_ONLY = 0x01,
+    MANUAL = 0x02,
+    AUTOMATIC = 0x03,
+}

android/app/aidl/android/bluetooth/aics/Mute.aidl
@@ -1,0 +1,29 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.bluetooth.aics;
+
+/**
+ * See Audio Input Control Service 1.0 - 2.2.1.2. Mute field
+ * {@hide}
+ */
+@JavaDerive(toString = true)
+@Backing(type="byte")
+enum Mute {
+    NOT_MUTED = 0x00,
+    MUTED = 0x01,
+    DISABLED = 0x02,
+}

notmikeb/aosp15_fluoride

common/bluetooth/constants/aics/AudioInputStatus.aidl
@@ -1,0 +1,29 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package bluetooth.constants.aics;
+
+/**
+ * See Audio Input Control Service 1.0 - 3.4 Audio Input Status
+ * {@hide}
+ */
+@JavaDerive(toString = true)
+@Backing(type="byte")
+enum AudioInputStatus {
+    INACTIVE = 0x00,
+    ACTIVE = 0x01,
+    // RFU 0x02 - 0xFF
+}

common/bluetooth/constants/aics/GainMode.aidl
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package bluetooth.constants.aics;
+
+/**
+ * See Audio Input Control Service 1.0 - 2.2.1.3. Gain_Mode field
+ * {@hide}
+ */
+@JavaDerive(toString = true)
+@Backing(type="byte")
+enum GainMode {
+    MANUAL_ONLY = 0x00,
+    AUTOMATIC_ONLY = 0x01,
+    MANUAL = 0x02,
+    AUTOMATIC = 0x03,
+}

common/bluetooth/constants/aics/Mute.aidl
@@ -1,0 +1,29 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package bluetooth.constants.aics;
+
+/**
+ * See Audio Input Control Service 1.0 - 2.2.1.2. Mute field
+ * {@hide}
+ */
+@JavaDerive(toString = true)
+@Backing(type="byte")
+enum Mute {
+    NOT_MUTED = 0x00,
+    MUTED = 0x01,
+    DISABLED = 0x02,
+}