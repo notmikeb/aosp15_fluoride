miyu-xu/packages-modules-Bluetooth

AICS: use aidl definition in java
Bug: 372328699
Flag: com.android.bluetooth.flags.aics_api
Test: m Bluetooth
Change-Id: I058961dcf9f33f1280fa2c112a323c09e106dbe4

android/app/src/com/android/bluetooth/vc/VolumeControlInputDescriptor.java
@@ -20,6 +20,9 @@ import android.util.Log;
 
 import com.android.bluetooth.btservice.ProfileService;
 
+import bluetooth.constants.AudioInputType;
+import bluetooth.constants.aics.AudioInputStatus;
+
 class VolumeControlInputDescriptor {
     private static final String TAG = VolumeControlInputDescriptor.class.getSimpleName();
 
@@ -33,9 +36,9 @@ class VolumeControlInputDescriptor {
     }
 
     private static class Descriptor {
-        int mStatus = 0; // AudioInputStatus.INACTIVE;
+        int mStatus = AudioInputStatus.INACTIVE;
 
-        int mType = 0; // AudioInputType.UNSPECIFIED;
+        int mType = AudioInputType.UNSPECIFIED;
 
         int mGainValue = 0;
 
@@ -83,7 +86,7 @@ class VolumeControlInputDescriptor {
     }
 
     int getStatus(int id) {
-        if (!validateId(id)) return 0; // AudioInputStatus.INACTIVE;
+        if (!validateId(id)) return AudioInputStatus.INACTIVE;
         return mVolumeInputs[id].mStatus;
     }
 
@@ -103,7 +106,7 @@ class VolumeControlInputDescriptor {
     }
 
     int getType(int id) {
-        if (!validateId(id)) return 0; // AudioInputType.UNSPECIFIED;
+        if (!validateId(id)) return AudioInputType.UNSPECIFIED;
         return mVolumeInputs[id].mType;
     }
 

notmikeb/aosp15_fluoride

android/app/src/com/android/bluetooth/vc/VolumeControlInputDescriptor.java
@@ -20,6 +20,11 @@ import android.util.Log;
 
 import com.android.bluetooth.btservice.ProfileService;
 
+import bluetooth.constants.AudioInputType;
+import bluetooth.constants.aics.AudioInputStatus;
+import bluetooth.constants.aics.GainMode;
+import bluetooth.constants.aics.Mute;
+
 class VolumeControlInputDescriptor {
     private static final String TAG = VolumeControlInputDescriptor.class.getSimpleName();
 
@@ -34,23 +39,15 @@ class VolumeControlInputDescriptor {
     }
 
     private static class Descriptor {
-        int mStatus = 0; // AudioInputStatus.INACTIVE;
+        int mStatus = AudioInputStatus.INACTIVE;
 
-        int mType = 0; // AudioInputType.UNSPECIFIED;
+        int mType = AudioInputType.UNSPECIFIED;
 
         int mGainValue = 0;
 
-        /* See AICS 1.0 - 3.1.3. Gain_Mode field
-         * The Gain_Mode field shall be set to a value that reflects whether gain modes are manual
-         * or automatic.
-         * - Manual Only, the server allows only manual gain.
-         * - Automatic Only, the server allows only automatic gain.
-         *
-         * For all other Gain_Mode field values, the server allows switchable automatic/manual gain.
-         */
-        int mGainMode = 0;
+        int mGainMode = GainMode.MANUAL_ONLY;
 
-        int mMute = 2; // DISABLED
+        int mMute = Mute.DISABLED;
 
         /* See AICS 1.0
          * The Gain_Setting (mGainValue) field is a signed value for which a single increment or
@@ -84,7 +81,7 @@ class VolumeControlInputDescriptor {
     }
 
     int getStatus(int id) {
-        if (!isValidId(id)) return 0; // AudioInputStatus.INACTIVE;
+        if (!isValidId(id)) return AudioInputStatus.INACTIVE;
         return mVolumeInputs[id].mStatus;
     }
 
@@ -104,7 +101,7 @@ class VolumeControlInputDescriptor {
     }
 
     int getType(int id) {
-        if (!isValidId(id)) return 0; // AudioInputType.UNSPECIFIED;
+        if (!isValidId(id)) return AudioInputType.UNSPECIFIED;
         return mVolumeInputs[id].mType;
     }
 
@@ -114,7 +111,7 @@ class VolumeControlInputDescriptor {
     }
 
     int getMute(int id) {
-        if (!isValidId(id)) return 2; // MuteField.DISABLED
+        if (!isValidId(id)) return Mute.DISABLED;
         return mVolumeInputs[id].mMute;
     }
 