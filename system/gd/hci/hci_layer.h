miyu-xu/packages-modules-Bluetooth

GD/HCI: Add fallback handler for vendor specific event
Bug: 360924438
Test: mma -j32
Flag: com.android.bluetooth.flags.hci_vendor_specific_extension
Change-Id: I095107e7d2d070799399a69a7d21bd83d6b1ab2e

system/gd/hci/hci_layer.h
@@ -84,6 +84,11 @@ public:
 
   virtual void UnregisterVendorSpecificEventHandler(VseSubeventCode event);
 
+  virtual void RegisterVendorSpecificEventDefaultHandler(
+          common::ContextualCallback<void(VendorSpecificEventView)> handler);
+
+  virtual void UnregisterVendorSpecificEventDefaultHandler();
+
   virtual void RegisterForDisconnects(
           common::ContextualCallback<void(uint16_t, hci::ErrorCode)> on_disconnect);
 

notmikeb/aosp15_fluoride

system/gd/hci/hci_layer.h
@@ -84,6 +84,11 @@ public:
 
   virtual void UnregisterVendorSpecificEventHandler(VseSubeventCode event);
 
+  virtual void RegisterDefaultVendorSpecificEventHandler(
+          common::ContextualCallback<void(VendorSpecificEventView)> handler);
+
+  virtual void UnregisterDefaultVendorSpecificEventHandler();
+
   virtual void RegisterForDisconnects(
           common::ContextualCallback<void(uint16_t, hci::ErrorCode)> on_disconnect);
 