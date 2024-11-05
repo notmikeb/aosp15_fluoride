miyu-xu/packages-modules-Bluetooth

AICS: Add native aics module
Bug: 372328699
Flag: com.android.bluetooth.flags.aics_api
Test: m .
Change-Id: I964c79c02cbf5b53aad1434b0361be4f18798677

android/app/jni/com_android_bluetooth_vc.cpp
@@ -17,6 +17,7 @@
 
 #define LOG_TAG "BluetoothVolumeControlServiceJni"
 
+#include <aics/api.h>
 #include <bluetooth/log.h>
 #include <jni.h>
 #include <nativehelper/JNIHelp.h>
@@ -34,9 +35,12 @@
 #include "hardware/bt_vc.h"
 #include "types/raw_address.h"
 
+using bluetooth::aics::MuteField;
 using bluetooth::vc::ConnectionState;
 using bluetooth::vc::VolumeControlCallbacks;
 using bluetooth::vc::VolumeControlInterface;
+using bluetooth::vc::VolumeInputStatus;
+using bluetooth::vc::VolumeInputType;
 
 namespace android {
 static jmethodID method_onConnectionStateChanged;
@@ -218,7 +222,7 @@ public:
   }
 
   void OnExtAudioInStateChanged(const RawAddress& bd_addr, uint8_t ext_input_id, int8_t gain_val,
-                                uint8_t gain_mode, uint8_t mute) override {
+                                uint8_t gain_mode, MuteField mute) override {
     log::info("");
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);
@@ -241,7 +245,7 @@ public:
   }
 
   void OnExtAudioInStatusChanged(const RawAddress& bd_addr, uint8_t ext_input_id,
-                                 bluetooth::vc::VolumeInputStatus status) override {
+                                 VolumeInputStatus status) override {
     log::info("");
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);
@@ -264,7 +268,7 @@ public:
   }
 
   void OnExtAudioInTypeChanged(const RawAddress& bd_addr, uint8_t ext_input_id,
-                               bluetooth::vc::VolumeInputType type) override {
+                               VolumeInputType type) override {
     log::info("");
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);

system/bta/aics/aics.cc
@@ -1,0 +1,33 @@
+// Copyright (C) 2024 The Android Open Source Project
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#define LOG_TAG "aics"
+
+#include <aics/api.h>
+#include <bluetooth/log.h>
+
+namespace bluetooth::aics {
+
+bool isAudioInputMuteField(uint8_t data) {
+  return data >= static_cast<uint8_t>(MuteField::NOT_MUTED) &&
+         data <= static_cast<uint8_t>(MuteField::DISABLED);
+}
+
+MuteField parseMuteField(uint8_t data) {
+  if (!isAudioInputMuteField(data)) {
+    log::fatal("{} is not a valid MuteField value", data);
+  }
+  return static_cast<MuteField>(data);
+}
+
+}  // namespace bluetooth::aics

system/bta/vc/vc.cc
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 
+#include <aics/api.h>
 #include <base/functional/bind.h>
 #include <base/strings/string_number_conversions.h>
 #include <base/strings/string_util.h>
@@ -583,11 +584,11 @@ public:
     STREAM_TO_INT8(input->gain_value, pp);
     uint8_t mute;
     STREAM_TO_UINT8(mute, pp);
-    if (mute > 0x02 /*Mute::DISABLED*/) {
+    if (!bluetooth::aics::isAudioInputMuteField(mute)) {
       bluetooth::log::error("{} Invalid mute value: {:#x}", device->address, mute);
       return;
     }
-    input->mute = mute;
+    input->mute = bluetooth::aics::parseMuteField(mute);
 
     STREAM_TO_UINT8(input->mode, pp);
     STREAM_TO_UINT8(input->change_counter, pp);

notmikeb/aosp15_fluoride

android/app/jni/com_android_bluetooth_vc.cpp
@@ -17,6 +17,7 @@
 
 #define LOG_TAG "BluetoothVolumeControlServiceJni"
 
+#include <aics/api.h>
 #include <bluetooth/log.h>
 #include <jni.h>
 #include <nativehelper/JNIHelp.h>
@@ -34,9 +35,12 @@
 #include "hardware/bt_vc.h"
 #include "types/raw_address.h"
 
+using bluetooth::aics::Mute;
 using bluetooth::vc::ConnectionState;
 using bluetooth::vc::VolumeControlCallbacks;
 using bluetooth::vc::VolumeControlInterface;
+using bluetooth::vc::VolumeInputStatus;
+using bluetooth::vc::VolumeInputType;
 
 namespace android {
 static jmethodID method_onConnectionStateChanged;
@@ -218,7 +222,7 @@ public:
   }
 
   void OnExtAudioInStateChanged(const RawAddress& bd_addr, uint8_t ext_input_id, int8_t gain_val,
-                                uint8_t gain_mode, uint8_t mute) override {
+                                uint8_t gain_mode, Mute mute) override {
     log::info("");
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);
@@ -241,7 +245,7 @@ public:
   }
 
   void OnExtAudioInStatusChanged(const RawAddress& bd_addr, uint8_t ext_input_id,
-                                 bluetooth::vc::VolumeInputStatus status) override {
+                                 VolumeInputStatus status) override {
     log::info("");
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);
@@ -264,7 +268,7 @@ public:
   }
 
   void OnExtAudioInTypeChanged(const RawAddress& bd_addr, uint8_t ext_input_id,
-                               bluetooth::vc::VolumeInputType type) override {
+                               VolumeInputType type) override {
     log::info("");
 
     std::shared_lock<std::shared_timed_mutex> lock(callbacks_mutex);

system/bta/aics/aics.cc
@@ -1,0 +1,32 @@
+// Copyright (C) 2024 The Android Open Source Project
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#define LOG_TAG "aics"
+
+#include <aics/api.h>
+#include <bluetooth/log.h>
+
+namespace bluetooth::aics {
+
+bool isValidAudioInputMuteValue(uint8_t data) {
+  return data >= static_cast<uint8_t>(Mute::NOT_MUTED) &&
+         data <= static_cast<uint8_t>(Mute::DISABLED);
+}
+
+Mute parseMuteField(uint8_t data) {
+  log::assert_that(isValidAudioInputMuteValue(data), "Not a valid Mute Value");
+
+  return static_cast<Mute>(data);
+}
+
+}  // namespace bluetooth::aics

system/bta/vc/vc.cc
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 
+#include <aics/api.h>
 #include <base/functional/bind.h>
 #include <base/strings/string_number_conversions.h>
 #include <base/strings/string_util.h>
@@ -583,11 +584,11 @@ public:
     STREAM_TO_INT8(input->gain_value, pp);
     uint8_t mute;
     STREAM_TO_UINT8(mute, pp);
-    if (mute > 0x02 /*Mute::DISABLED*/) {
+    if (!bluetooth::aics::isValidAudioInputMuteValue(mute)) {
       bluetooth::log::error("{} Invalid mute value: {:#x}", device->address, mute);
       return;
     }
-    input->mute = mute;
+    input->mute = bluetooth::aics::parseMuteField(mute);
 
     STREAM_TO_UINT8(input->mode, pp);
     STREAM_TO_UINT8(input->change_counter, pp);