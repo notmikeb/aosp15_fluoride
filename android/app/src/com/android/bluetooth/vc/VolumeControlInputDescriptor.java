miyu-xu/packages-modules-Bluetooth

AICS: getStatus/onStatusChanged implementation
Bug: 372328699
Flag: com.android.bluetooth.flags.aics_api
Test: atest CtsBluetoothTestCases
Change-Id: I0218ef71ac2642c495598ddf4e9d40891906c758

android/app/src/com/android/bluetooth/vc/VolumeControlInputDescriptor.java
@@ -20,6 +20,7 @@ import static com.android.bluetooth.Utils.RemoteExceptionIgnoringConsumer;
 
 import static java.util.Objects.requireNonNull;
 
+import android.bluetooth.AudioInputControl.Status;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.IAudioInputCallback;
 import android.os.RemoteCallbackList;
@@ -53,7 +54,7 @@ class VolumeControlInputDescriptor {
     }
 
     private static class Descriptor {
-        int mStatus = AudioInputStatus.INACTIVE;
+        @Status int mStatus = AudioInputStatus.INACTIVE;
 
         int mType = AudioInputType.UNSPECIFIED;
 
@@ -121,9 +122,10 @@ class VolumeControlInputDescriptor {
         mVolumeInputs[id].unregisterCallback(callback);
     }
 
-    void setStatus(int id, int status) {
+    void onStatusChanged(int id, @Status int status) {
         if (!isValidId(id)) return;
         mVolumeInputs[id].mStatus = status;
+        mVolumeInputs[id].broadcast("onStatusChanged", (c) -> c.onStatusChanged(status));
     }
 
     int getStatus(int id) {

android/app/src/com/android/bluetooth/vc/VolumeControlNativeCallback.java
@@ -25,6 +25,7 @@ import static com.android.bluetooth.vc.VolumeControlStackEvent.EVENT_TYPE_VOLUME
 
 import static java.util.Objects.requireNonNull;
 
+import android.bluetooth.AudioInputControl.Status;
 import android.bluetooth.BluetoothDevice;
 import android.util.Log;
 
@@ -159,7 +160,7 @@ class VolumeControlNativeCallback {
     }
 
     @VisibleForTesting
-    void onExtAudioInStatusChanged(int id, int status, byte[] address) {
+    void onExtAudioInStatusChanged(int id, @Status int status, byte[] address) {
         sendMessageToService(s -> s.onExtAudioInStatusChanged(getDevice(address), id, status));
     }
 

notmikeb/aosp15_fluoride

android/app/src/com/android/bluetooth/vc/VolumeControlInputDescriptor.java
@@ -20,6 +20,7 @@ import static com.android.bluetooth.Utils.RemoteExceptionIgnoringConsumer;
 
 import static java.util.Objects.requireNonNull;
 
+import android.bluetooth.AudioInputControl.AudioInputStatus;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.IAudioInputCallback;
 import android.os.RemoteCallbackList;
@@ -28,7 +29,6 @@ import android.util.Log;
 import com.android.bluetooth.btservice.ProfileService;
 
 import bluetooth.constants.AudioInputType;
-import bluetooth.constants.aics.AudioInputStatus;
 import bluetooth.constants.aics.GainMode;
 import bluetooth.constants.aics.Mute;
 
@@ -53,7 +53,7 @@ class VolumeControlInputDescriptor {
     }
 
     private static class Descriptor {
-        int mStatus = AudioInputStatus.INACTIVE;
+        @AudioInputStatus int mStatus = bluetooth.constants.aics.AudioInputStatus.INACTIVE;
 
         int mType = AudioInputType.UNSPECIFIED;
 
@@ -121,13 +121,14 @@ class VolumeControlInputDescriptor {
         mVolumeInputs[id].unregisterCallback(callback);
     }
 
-    void setStatus(int id, int status) {
+    void onStatusChanged(int id, @AudioInputStatus int status) {
         if (!isValidId(id)) return;
         mVolumeInputs[id].mStatus = status;
+        mVolumeInputs[id].broadcast("onStatusChanged", (c) -> c.onStatusChanged(status));
     }
 
     int getStatus(int id) {
-        if (!isValidId(id)) return AudioInputStatus.INACTIVE;
+        if (!isValidId(id)) return bluetooth.constants.aics.AudioInputStatus.INACTIVE;
         return mVolumeInputs[id].mStatus;
     }
 

android/app/src/com/android/bluetooth/vc/VolumeControlNativeCallback.java
@@ -25,6 +25,7 @@ import static com.android.bluetooth.vc.VolumeControlStackEvent.EVENT_TYPE_VOLUME
 
 import static java.util.Objects.requireNonNull;
 
+import android.bluetooth.AudioInputControl.AudioInputStatus;
 import android.bluetooth.BluetoothDevice;
 import android.util.Log;
 
@@ -159,7 +160,7 @@ class VolumeControlNativeCallback {
     }
 
     @VisibleForTesting
-    void onExtAudioInStatusChanged(int id, int status, byte[] address) {
+    void onExtAudioInStatusChanged(int id, @AudioInputStatus int status, byte[] address) {
         sendMessageToService(s -> s.onExtAudioInStatusChanged(getDevice(address), id, status));
     }
 