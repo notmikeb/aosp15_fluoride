0ba75ae1ea6268d471314e178cc6618bf2352269
Rishab <rghanti@google.com>
2024-11-14 01:47:00 +0000

    Improve Bluetooth logging to include all remote devices interacting with Bluetooth
    Bug: 378971581
    Test: m com.android.btservices and statsd_testdrive
    Flag: EXEMPT, Metrics Logging
    Change-Id: I82cc34f2501dc39a05b65c65c2775bfb4e29892d

--- android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
+++ android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -50,6 +50,7 @@ import com.android.bluetooth.bas.BatteryService;
 import com.android.bluetooth.flags.Flags;
 import com.android.bluetooth.hfp.HeadsetHalConstants;
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.modules.utils.build.SdkLevel;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayDeque;
@@ -946,6 +947,12 @@ public class RemoteDevices {
                                 break;
                             }
                             deviceProperties.setName(newName);
+                            List<String> wordBreakdownList =
+                                    MetricsLogger.getInstance().getWordBreakdownList(newName);
+                            if (SdkLevel.isAtLeastU()) {
+                                MetricsLogger.getInstance()
+                                        .uploadRestrictedBluetothDeviceName(wordBreakdownList);
+                            }
                             intent = new Intent(BluetoothDevice.ACTION_NAME_CHANGED);
                             intent.putExtra(BluetoothDevice.EXTRA_DEVICE, bdDevice);
                             intent.putExtra(BluetoothDevice.EXTRA_NAME, deviceProperties.getName());
@@ -1294,6 +1301,13 @@ public class RemoteDevices {
                 mAdapterService.obfuscateAddress(device),
                 getBluetoothClass(device),
                 metricId);
+
+        byte[] remoteDeviceInfoBytes = MetricsLogger.getInstance().getRemoteDeviceInfoProto(device);
+
+        BluetoothStatsLog.write(
+                BluetoothStatsLog.REMOTE_DEVICE_INFORMATION_WITH_METRIC_ID,
+                metricId,
+                remoteDeviceInfoBytes);
 
         if (intent == null) {
             Log.e(TAG, "aclStateChangeCallback intent is null. BondState: " + getBondState(device));
