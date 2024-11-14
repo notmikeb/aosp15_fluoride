miyu-xu/packages-modules-Bluetooth

system/bta: Apply clang-tidy fixes
Bug: 374984156
Test: m libbt-bta-tidy
Test: m com.android.btservices
Flag: EXEMPT, no logical change
Change-Id: I8bbe181731b5cc69f6a9de5dfd2d9ef332ecde8d

system/bta/av/bta_av_aact.cc
@@ -34,19 +34,40 @@
 #include <cstring>
 #include <vector>
 
+#include "a2dp_api.h"
+#include "a2dp_codec_api.h"
+#include "a2dp_constants.h"
+#include "a2dp_sbc_constants.h"
+#include "audio_hal_interface/a2dp_encoding.h"
+#include "avdt_api.h"
+#include "avrc_api.h"
+#include "avrc_defs.h"
+#include "bt_name.h"
+#include "bt_transport.h"
 #include "bta/av/bta_av_int.h"
-#include "bta/include/bta_ar_api.h"
 #include "bta/include/bta_av_co.h"
+#include "bta_av_api.h"
+#include "bta_sys.h"
 #include "btif/avrcp/avrcp_service.h"
 #include "btif/include/btif_av.h"
 #include "btif/include/btif_av_co.h"
 #include "btif/include/btif_config.h"
 #include "btif/include/btif_storage.h"
+#include "btm_api_types.h"
+#include "common/bind.h"
+#include "device/include/device_iot_conf_defs.h"
 #include "device/include/device_iot_config.h"
 #include "device/include/interop.h"
+#include "hardware/bt_av.h"
+#include "hci_error_code.h"
+#include "hcidefs.h"
 #include "internal_include/bt_target.h"
+#include "l2cap_types.h"
+#include "osi/include/alarm.h"
 #include "osi/include/allocator.h"
+#include "osi/include/list.h"
 #include "osi/include/properties.h"
+#include "sdpdefs.h"
 #include "stack/include/a2dp_ext.h"
 #include "stack/include/a2dp_sbc.h"
 #include "stack/include/acl_api.h"

system/bta/csis/csis_client.cc
@@ -21,30 +21,49 @@
 #include <bluetooth/log.h>
 #include <hardware/bt_csis.h>
 #include <hardware/bt_gatt_types.h>
+#include <stdio.h>
 
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <limits>
 #include <list>
+#include <map>
+#include <memory>
 #include <mutex>
+#include <sstream>
 #include <string>
+#include <type_traits>
+#include <utility>
 #include <vector>
 
 #include "advertise_data_parser.h"
+#include "bluetooth/uuid.h"
+#include "bt_transport.h"
 #include "bta_api.h"
 #include "bta_csis_api.h"
 #include "bta_gatt_api.h"
 #include "bta_gatt_queue.h"
 #include "bta_groups.h"
-#include "bta_le_audio_uuids.h"
 #include "bta_sec_api.h"
 #include "btif/include/btif_storage.h"
+#include "btm_ble_api_types.h"
+#include "btm_sec_api_types.h"
 #include "crypto_toolbox/crypto_toolbox.h"
 #include "csis_types.h"
 #include "gap_api.h"
+#include "gatt/database.h"
 #include "gatt_api.h"
+#include "gattdefs.h"
 #include "internal_include/bt_target.h"
 #include "internal_include/bt_trace.h"
 #include "main/shim/le_scanning_manager.h"
+#include "neighbor_inquiry.h"
+#include "os/logging/log_adapter.h"
 #include "osi/include/osi.h"
 #include "osi/include/stack_power_telemetry.h"
+#include "raw_address.h"
 #include "stack/btm/btm_sec.h"
 #include "stack/gatt/gatt_int.h"
 #include "stack/include/bt_types.h"

system/bta/has/has_client.cc
@@ -22,24 +22,44 @@
 #include <com_android_bluetooth_flags.h>
 #include <hardware/bt_gatt_types.h>
 #include <hardware/bt_has.h>
+#include <stdio.h>
 
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
 #include <list>
 #include <map>
 #include <mutex>
+#include <optional>
+#include <sstream>
 #include <string>
+#include <utility>
+#include <variant>
 #include <vector>
 
+#include "bluetooth/uuid.h"
+#include "bt_transport.h"
 #include "bta_csis_api.h"
 #include "bta_gatt_api.h"
 #include "bta_gatt_queue.h"
 #include "bta_has_api.h"
 #include "bta_le_audio_uuids.h"
+#include "btm_ble_api_types.h"
 #include "btm_sec.h"
+#include "btm_sec_api_types.h"
+#include "btm_status.h"
 #include "gap_api.h"
+#include "gatt/database.h"
 #include "gatt_api.h"
+#include "gattdefs.h"
+#include "has_ctp.h"
+#include "has_journal.h"
+#include "has_preset.h"
 #include "has_types.h"
-#include "internal_include/bt_trace.h"
+#include "osi/include/alarm.h"
 #include "osi/include/properties.h"
+#include "raw_address.h"
 #include "stack/include/bt_types.h"
 
 using base::Closure;
@@ -47,8 +67,6 @@ using bluetooth::Uuid;
 using bluetooth::csis::CsisClient;
 using bluetooth::has::ConnectionState;
 using bluetooth::has::ErrorCode;
-using bluetooth::has::kFeatureBitPresetSynchronizationSupported;
-using bluetooth::has::kHasPresetIndexInvalid;
 using bluetooth::has::PresetInfo;
 using bluetooth::has::PresetInfoReason;
 using bluetooth::le_audio::has::HasClient;
@@ -59,8 +77,6 @@ using bluetooth::le_audio::has::HasDevice;
 using bluetooth::le_audio::has::HasGattOpContext;
 using bluetooth::le_audio::has::HasJournalRecord;
 using bluetooth::le_audio::has::HasPreset;
-using bluetooth::le_audio::has::kControlPointMandatoryOpcodesBitmask;
-using bluetooth::le_audio::has::kControlPointSynchronizedOpcodesBitmask;
 using bluetooth::le_audio::has::kUuidActivePresetIndex;
 using bluetooth::le_audio::has::kUuidHearingAccessService;
 using bluetooth::le_audio::has::kUuidHearingAidFeatures;

notmikeb/aosp15_fluoride

system/bta/av/bta_av_aact.cc
@@ -34,19 +34,39 @@
 #include <cstring>
 #include <vector>
 
+#include "a2dp_api.h"
+#include "a2dp_codec_api.h"
+#include "a2dp_constants.h"
+#include "a2dp_sbc_constants.h"
+#include "audio_hal_interface/a2dp_encoding.h"
+#include "avdt_api.h"
+#include "avrc_api.h"
+#include "avrc_defs.h"
+#include "bt_name.h"
 #include "bta/av/bta_av_int.h"
-#include "bta/include/bta_ar_api.h"
 #include "bta/include/bta_av_co.h"
+#include "bta_av_api.h"
+#include "bta_sys.h"
 #include "btif/avrcp/avrcp_service.h"
 #include "btif/include/btif_av.h"
 #include "btif/include/btif_av_co.h"
 #include "btif/include/btif_config.h"
 #include "btif/include/btif_storage.h"
+#include "btm_api_types.h"
+#include "common/message_loop_thread.h"
+#include "device/include/device_iot_conf_defs.h"
 #include "device/include/device_iot_config.h"
 #include "device/include/interop.h"
+#include "hardware/bt_av.h"
+#include "hci_error_code.h"
+#include "hcidefs.h"
 #include "internal_include/bt_target.h"
+#include "l2cap_types.h"
+#include "osi/include/alarm.h"
 #include "osi/include/allocator.h"
+#include "osi/include/list.h"
 #include "osi/include/properties.h"
+#include "sdpdefs.h"
 #include "stack/include/a2dp_ext.h"
 #include "stack/include/a2dp_sbc.h"
 #include "stack/include/acl_api.h"
@@ -58,6 +78,7 @@
 #include "stack/include/btm_status.h"
 #include "stack/include/l2cap_interface.h"
 #include "storage/config_keys.h"
+#include "types/bt_transport.h"
 #include "types/hci_role.h"
 #include "types/raw_address.h"
 

system/bta/csis/csis_client.cc
@@ -21,10 +21,21 @@
 #include <bluetooth/log.h>
 #include <hardware/bt_csis.h>
 #include <hardware/bt_gatt_types.h>
+#include <stdio.h>
 
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <limits>
 #include <list>
+#include <map>
+#include <memory>
 #include <mutex>
+#include <sstream>
 #include <string>
+#include <type_traits>
+#include <utility>
 #include <vector>
 
 #include "advertise_data_parser.h"
@@ -33,16 +44,21 @@
 #include "bta_gatt_api.h"
 #include "bta_gatt_queue.h"
 #include "bta_groups.h"
-#include "bta_le_audio_uuids.h"
 #include "bta_sec_api.h"
 #include "btif/include/btif_storage.h"
+#include "btm_ble_api_types.h"
+#include "btm_sec_api_types.h"
 #include "crypto_toolbox/crypto_toolbox.h"
 #include "csis_types.h"
 #include "gap_api.h"
+#include "gatt/database.h"
 #include "gatt_api.h"
+#include "gattdefs.h"
 #include "internal_include/bt_target.h"
 #include "internal_include/bt_trace.h"
 #include "main/shim/le_scanning_manager.h"
+#include "neighbor_inquiry.h"
+#include "os/logging/log_adapter.h"
 #include "osi/include/osi.h"
 #include "osi/include/stack_power_telemetry.h"
 #include "stack/btm/btm_sec.h"
@@ -51,6 +67,9 @@
 #include "stack/include/btm_ble_sec_api.h"
 #include "stack/include/btm_client_interface.h"
 #include "stack/include/btm_status.h"
+#include "types/bluetooth/uuid.h"
+#include "types/bt_transport.h"
+#include "types/raw_address.h"
 
 using base::Closure;
 using bluetooth::Uuid;

system/bta/has/has_client.cc
@@ -22,11 +22,20 @@
 #include <com_android_bluetooth_flags.h>
 #include <hardware/bt_gatt_types.h>
 #include <hardware/bt_has.h>
+#include <stdio.h>
 
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
 #include <list>
 #include <map>
 #include <mutex>
+#include <optional>
+#include <sstream>
 #include <string>
+#include <utility>
+#include <variant>
 #include <vector>
 
 #include "bta_csis_api.h"
@@ -34,21 +43,30 @@
 #include "bta_gatt_queue.h"
 #include "bta_has_api.h"
 #include "bta_le_audio_uuids.h"
+#include "btm_ble_api_types.h"
 #include "btm_sec.h"
+#include "btm_sec_api_types.h"
+#include "btm_status.h"
 #include "gap_api.h"
+#include "gatt/database.h"
 #include "gatt_api.h"
+#include "gattdefs.h"
+#include "has_ctp.h"
+#include "has_journal.h"
+#include "has_preset.h"
 #include "has_types.h"
-#include "internal_include/bt_trace.h"
+#include "osi/include/alarm.h"
 #include "osi/include/properties.h"
 #include "stack/include/bt_types.h"
+#include "types/bluetooth/uuid.h"
+#include "types/bt_transport.h"
+#include "types/raw_address.h"
 
 using base::Closure;
 using bluetooth::Uuid;
 using bluetooth::csis::CsisClient;
 using bluetooth::has::ConnectionState;
 using bluetooth::has::ErrorCode;
-using bluetooth::has::kFeatureBitPresetSynchronizationSupported;
-using bluetooth::has::kHasPresetIndexInvalid;
 using bluetooth::has::PresetInfo;
 using bluetooth::has::PresetInfoReason;
 using bluetooth::le_audio::has::HasClient;
@@ -59,8 +77,6 @@ using bluetooth::le_audio::has::HasDevice;
 using bluetooth::le_audio::has::HasGattOpContext;
 using bluetooth::le_audio::has::HasJournalRecord;
 using bluetooth::le_audio::has::HasPreset;
-using bluetooth::le_audio::has::kControlPointMandatoryOpcodesBitmask;
-using bluetooth::le_audio::has::kControlPointSynchronizedOpcodesBitmask;
 using bluetooth::le_audio::has::kUuidActivePresetIndex;
 using bluetooth::le_audio::has::kUuidHearingAccessService;
 using bluetooth::le_audio::has::kUuidHearingAidFeatures;