miyu-xu/packages-modules-Bluetooth

pass CS config parameters to hal
Flag: com.android.bluetooth.flags.channel_sounding_25q2_apis
Bug: 367409858
Bug: 357090692

Test: com.android.btservices

Change-Id: I4698302dd2f2e1c7788b72d3ed2c60eac2235c65

system/gd/hal/ranging_hal.h
@@ -18,11 +18,11 @@
 
 #include <complex>
 
+#include "hci/hci_packets.h"
 #include "module.h"
 
 namespace bluetooth {
 namespace hal {
-
 enum RangingHalVersion {
   V_UNKNOWN = 0,
   V_1 = 1,
@@ -70,6 +70,7 @@ public:
 
   virtual ~RangingHal() = default;
   virtual bool IsBound() = 0;
+  virtual RangingHalVersion GetRangingHalVersion() = 0;
   virtual void RegisterCallback(RangingHalCallback* callback) = 0;
   virtual std::vector<VendorSpecificCharacteristic> GetVendorSpecificCharacteristics() = 0;
   virtual void OpenSession(
@@ -79,6 +80,12 @@ public:
           uint16_t connection_handle,
           const std::vector<hal::VendorSpecificCharacteristic>& vendor_specific_reply) = 0;
   virtual void WriteRawData(uint16_t connection_handle, const ChannelSoundingRawData& raw_data) = 0;
+  virtual void UpdateChannelSoundingConfig(
+          uint16_t connection_handle,
+          const hci::LeCsConfigCompleteView& leCsConfigCompleteView) = 0;
+  virtual void UpdatePrecedureEnableConfig(
+          uint16_t connection_handle,
+          const hci::LeCsProcedureEnableCompleteView& leCsProcedureEnableCompleteView) = 0;
 };
 
 }  // namespace hal

system/gd/hci/distance_measurement_manager.cc
@@ -906,6 +906,10 @@ struct DistanceMeasurementManager::impl : bluetooth::hal::RangingHalCallback {
     live_tracker->main_mode_type = event_view.GetMainModeType();
     live_tracker->sub_mode_type = event_view.GetSubModeType();
     live_tracker->rtt_type = event_view.GetRttType();
+    if (com::android::bluetooth::flags::channel_sounding_25q2_apis() && live_tracker->local_start &&
+        ranging_hal_->GetRangingHalVersion() == hal::V_2) {
+      ranging_hal_->UpdateChannelSoundingConfig(connection_handle, event_view);
+    }
     if (live_tracker->local_hci_role == hci::Role::CENTRAL) {
       // send the cmd from the BLE central only.
       send_le_cs_security_enable(connection_handle);
@@ -1033,6 +1037,10 @@ struct DistanceMeasurementManager::impl : bluetooth::hal::RangingHalCallback {
         live_tracker->waiting_for_start_callback = false;
         distance_measurement_callbacks_->OnDistanceMeasurementStarted(live_tracker->address,
                                                                       METHOD_CS);
+      }
+      if (com::android::bluetooth::flags::channel_sounding_25q2_apis() &&
+          live_tracker->local_start && ranging_hal_->GetRangingHalVersion() == hal::V_2) {
+        ranging_hal_->UpdatePrecedureEnableConfig(connection_handle, event_view);
       }
     } else if (event_view.GetState() == Enable::DISABLED) {
       uint8_t valid_requester_states = static_cast<uint8_t>(CsTrackerState::STARTED);

notmikeb/aosp15_fluoride

Test: m com.android.btservices

system/gd/hal/ranging_hal.h
@@ -18,11 +18,11 @@
 
 #include <complex>
 
+#include "hci/hci_packets.h"
 #include "module.h"
 
 namespace bluetooth {
 namespace hal {
-
 enum RangingHalVersion {
   V_UNKNOWN = 0,
   V_1 = 1,
@@ -70,6 +70,7 @@ public:
 
   virtual ~RangingHal() = default;
   virtual bool IsBound() = 0;
+  virtual RangingHalVersion GetRangingHalVersion() = 0;
   virtual void RegisterCallback(RangingHalCallback* callback) = 0;
   virtual std::vector<VendorSpecificCharacteristic> GetVendorSpecificCharacteristics() = 0;
   virtual void OpenSession(
@@ -79,6 +80,12 @@ public:
           uint16_t connection_handle,
           const std::vector<hal::VendorSpecificCharacteristic>& vendor_specific_reply) = 0;
   virtual void WriteRawData(uint16_t connection_handle, const ChannelSoundingRawData& raw_data) = 0;
+  virtual void UpdateChannelSoundingConfig(
+          uint16_t connection_handle,
+          const hci::LeCsConfigCompleteView& leCsConfigCompleteView) = 0;
+  virtual void UpdateProcedureEnableConfig(
+          uint16_t connection_handle,
+          const hci::LeCsProcedureEnableCompleteView& leCsProcedureEnableCompleteView) = 0;
 };
 
 }  // namespace hal

system/gd/hci/distance_measurement_manager.cc
@@ -939,6 +939,10 @@ struct DistanceMeasurementManager::impl : bluetooth::hal::RangingHalCallback {
     live_tracker->main_mode_type = event_view.GetMainModeType();
     live_tracker->sub_mode_type = event_view.GetSubModeType();
     live_tracker->rtt_type = event_view.GetRttType();
+    if (com::android::bluetooth::flags::channel_sounding_25q2_apis() && live_tracker->local_start &&
+        ranging_hal_->GetRangingHalVersion() == hal::V_2) {
+      ranging_hal_->UpdateChannelSoundingConfig(connection_handle, event_view);
+    }
     if (live_tracker->local_hci_role == hci::Role::CENTRAL) {
       // send the cmd from the BLE central only.
       send_le_cs_security_enable(connection_handle);
@@ -1090,6 +1094,10 @@ struct DistanceMeasurementManager::impl : bluetooth::hal::RangingHalCallback {
         live_tracker->waiting_for_start_callback = false;
         distance_measurement_callbacks_->OnDistanceMeasurementStarted(live_tracker->address,
                                                                       METHOD_CS);
+      }
+      if (com::android::bluetooth::flags::channel_sounding_25q2_apis() &&
+          live_tracker->local_start && ranging_hal_->GetRangingHalVersion() == hal::V_2) {
+        ranging_hal_->UpdateProcedureEnableConfig(connection_handle, event_view);
       }
     } else if (event_view.GetState() == Enable::DISABLED) {
       uint8_t valid_requester_states = static_cast<uint8_t>(CsTrackerState::STARTED);