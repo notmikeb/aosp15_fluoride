miyu-xu/packages-modules-Bluetooth

Set all-zero of peer address, peer address type, and peer IRK in resolving list for RPA offload
Bug: 378398874
Bug: 378143579
Test: mm packages/modules/Bluetooth
Test: test with vendor test firmware b/331772841
Flag: com.android.bluetooth.flags.rpa_offload_to_bt_controller
Change-Id: I1dcb197f263fce9266dfdf802ec4d16f1e2dc613

system/gd/hci/controller.cc
@@ -1546,7 +1546,7 @@ uint64_t Controller::MaskLeEventMask(HciVersion version, uint64_t mask) {
   }
 }
 
-bool Controller::IsRpaOffloadSupported(void) {
+bool Controller::IsRpaOffloadSupported(void) const {
   static const bool rpa_supported =
           com::android::bluetooth::flags::rpa_offload_to_bt_controller() &&
           os::GetSystemPropertyBool(kPropertyRpaOffload, kDefaultRpaOffload) &&

system/gd/hci/controller.h
@@ -217,7 +217,7 @@ public:
 
   static uint64_t MaskLeEventMask(HciVersion version, uint64_t mask);
 
-  bool IsRpaOffloadSupported(void);
+  virtual bool IsRpaOffloadSupported(void) const override;
 
 protected:
   void ListDependencies(ModuleList* list) const override;

system/gd/hci/controller_interface.h
@@ -215,6 +215,8 @@ public:
   virtual VendorCapabilities GetVendorCapabilities() const = 0;
 
   virtual bool IsSupported(OpCode op_code) const = 0;
+
+  virtual bool IsRpaOffloadSupported(void) const = 0;
 };
 
 }  // namespace hci

notmikeb/aosp15_fluoride

system/gd/hci/controller.cc
@@ -1546,7 +1546,7 @@ uint64_t Controller::MaskLeEventMask(HciVersion version, uint64_t mask) {
   }
 }
 
-bool Controller::IsRpaOffloadSupported(void) {
+bool Controller::IsRpaGenerationSupported(void) const {
   static const bool rpa_supported =
           com::android::bluetooth::flags::rpa_offload_to_bt_controller() &&
           os::GetSystemPropertyBool(kPropertyRpaOffload, kDefaultRpaOffload) &&

system/gd/hci/controller.h
@@ -217,7 +217,7 @@ public:
 
   static uint64_t MaskLeEventMask(HciVersion version, uint64_t mask);
 
-  bool IsRpaOffloadSupported(void);
+  virtual bool IsRpaGenerationSupported(void) const override;
 
 protected:
   void ListDependencies(ModuleList* list) const override;

system/gd/hci/controller_interface.h
@@ -215,6 +215,8 @@ public:
   virtual VendorCapabilities GetVendorCapabilities() const = 0;
 
   virtual bool IsSupported(OpCode op_code) const = 0;
+
+  virtual bool IsRpaGenerationSupported(void) const = 0;
 };
 
 }  // namespace hci